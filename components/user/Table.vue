21b3cd889f67cb3fbac980b70d74c823eda4b3bf
SahaphapM <65160309@go.buu.ac.th>
2025-02-21 08:47:53 +0700

    adjust user table

--- components/user/Table.vue
+++ components/user/Table.vue
@@ -1,23 +1,18 @@
 <template>
-  <div
-    class="overflow-x-auto bg-base-100 border border-gray-200 rounded-lg p-4"
-  >
-    <!-- Search Input -->
-    <div class="flex justify-between mb-4">
-      <input
-        v-model="userStore.query.search"
-        placeholder="Search Users..."
-        class="input input-bordered w-64"
-      />
-    </div>
-
+  <div class="table-container">
     <!-- Table -->
-    <table class="table w-full">
+    <table class="custom-table">
       <thead>
-        <tr class="bg-base-300 text-base-content text-lg">
-          <th @click="setSorting('id')" class="cursor-pointer">ID</th>
-          <th @click="setSorting('name')" class="cursor-pointer">Name</th>
-          <th @click="setSorting('email')" class="cursor-pointer">Email</th>
+        <tr>
+          <th @click="setSorting('id')" class="cursor-pointer">
+            <div class="flex">ID. <SortDescIcon class="sort-icon" /></div>
+          </th>
+          <th @click="setSorting('name')" class="cursor-pointer">
+            <div class="flex">Name <SortDescIcon class="sort-icon" /></div>
+          </th>
+          <th @click="setSorting('email')" class="cursor-pointer">
+            <div class="flex">Email <SortDescIcon class="sort-icon" /></div>
+          </th>
           <th>Position</th>
           <th>Role</th>
           <th class="text-center">Actions</th>
@@ -48,12 +43,15 @@
             }}</span>
           </td>
           <td class="text-center">
-            <button @click="$emit('edit', user)" class="btn btn-warning btn-sm">
+            <button
+              @click="$emit('edit', user)"
+              class="btn btn-warning btn-sm w-16"
+            >
               Edit
             </button>
             <button
               @click="$emit('delete', user.id)"
-              class="btn btn-error btn-sm ml-2"
+              class="btn btn-error btn-sm ml-2 w-16"
             >
               Delete
             </button>
@@ -61,29 +59,26 @@
         </tr>
       </tbody>
     </table>
-
     <!-- Pagination Controls -->
-    <div class="flex justify-between items-center mt-4">
-      <button
-        @click="prevPage"
-        :disabled="userStore.query.page === 1"
-        class="btn btn-primary"
-      >
-        Previous
+    <div class="pagination">
+      <button @click="prevPage" :disabled="userStore.query.page === 1">
+        <ChevronLeft />
       </button>
-      <span>Page {{ userStore.query.page }} of {{ userStore.totalPages }}</span>
+
+      <span> {{ userStore.query.page }} / {{ userStore.totalPages }} </span>
+
       <button
         @click="nextPage"
         :disabled="userStore.query.page === userStore.totalPages"
-        class="btn btn-primary"
       >
-        Next
+        <ChevronRight />
       </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ChevronLeft, ChevronRight, SortDescIcon } from "lucide-vue-next";
 import { defineProps, defineEmits } from "vue";
 import useServiceUsers from "~/composables/usersService";
 defineEmits(["edit", "delete"]);
@@ -94,7 +89,18 @@ const userStore = useUserStore();
 let debounceTimeout = ref<NodeJS.Timeout | null>(null);
 
 watch(
-  () => userStore.query.search, // Watch only the search input
+  () => userStore.query.search || userStore.query.sortBy, // Watch only the search input
+  (newValue) => {
+    if (debounceTimeout.value) clearTimeout(debounceTimeout.value); // Clear previous timeout
+
+    debounceTimeout.value = setTimeout(() => {
+      userStore.getUsers(); // Fetch users after 2s delay
+    }, 500);
+  }
+);
+
+watch(
+  () => userStore.query.order, // Watch only the search input
   (newValue) => {
     if (debounceTimeout.value) clearTimeout(debounceTimeout.value); // Clear previous timeout
 
@@ -108,6 +114,7 @@ watch(
 const setSorting = (field: string) => {
   userStore.query.sortBy = field;
   userStore.query.order = userStore.query.order === "ASC" ? "DESC" : "ASC";
+  console.log("userStore.query", userStore.query.sortBy);
 };
 
 // Pagination functions
@@ -125,3 +132,96 @@ onMounted(async () => {
   });
 });
 </script>
+
+<style scoped>
+/* Container */
+.table-container {
+  overflow-x: auto;
+  background: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* Table */
+.custom-table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+
+/* Table Header */
+.custom-table thead tr {
+  background: rgb(231, 231, 231);
+
+  text-align: left;
+  font-size: 15px;
+  color: #1d1d1d;
+}
+
+.custom-table th {
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  font-weight: 600;
+  border-bottom: 2px solid #ddd;
+}
+
+/* Table Body */
+.custom-table td {
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #e5e7eb;
+}
+
+/* Alignments */
+.text-center {
+  text-align: left;
+}
+.text-right {
+  text-align: right;
+}
+.truncate {
+  max-width: 150px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+/* Row Hover Effect */
+.custom-table tbody tr:hover {
+  background: #f9fafb;
+  cursor: pointer;
+}
+/* Pagination */
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  padding: 10px;
+}
+
+.pagination button {
+  padding: 6px 10px;
+  font-size: 12px;
+  border: none;
+  cursor: pointer;
+  background: rgb(207, 207, 207);
+  color: #7c7c7c;
+  border-radius: 4px;
+}
+.pagination button:disabled {
+  background: #f0f0f0;
+  cursor: not-allowed;
+}
+.sort-icon {
+  color: #5a5a5a;
+  width: 15px;
+  height: 15px;
+  margin-top: 5px;
+  margin-left: 4px;
+}
+</style>
